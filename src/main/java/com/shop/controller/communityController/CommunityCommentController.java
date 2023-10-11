1ab79a278ffbe09bc09b9dc585d546484cf83f05
tjrqh <asdww6631@gmail.com>
2023-10-11 10:32:26 +0900

    community search Comment search implement

--- src/main/java/com/shop/controller/communityController/CommunityCommentController.java
+++ src/main/java/com/shop/controller/communityController/CommunityCommentController.java
@@ -1,25 +1,26 @@
 package com.shop.controller.communityController;
 
 
-import com.shop.dto.communityDto.commentDto.CommentReadResponse;
+import com.shop.entity.cummunityEntity.Post;
 import com.shop.service.communityService.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+
 public class CommunityCommentController {
 
     @Autowired
     private CommentService commentService;
 
-    @GetMapping
-    public List<CommentReadResponse> findByComment(@RequestParam(value = "q")final String comment){
-        return commentService.
+
+    @GetMapping("/comment")
+    public List<Post> findByComment(@RequestParam(value = "q")final String comment){
+        return commentService.findByComment(comment);
     }
 
 }
